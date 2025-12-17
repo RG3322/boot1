c44a0892edfc2a07f77297926dbeacd05ebd4606
RG3322 <raghavsankhyaan@gmail.com>
2025-12-17 17:52:39 +0530

    pom.xml means maven project

---
+++ boot1/src/main/java/com/example/boot1/Explanation/Explanation.java
@@ -0,0 +1,9 @@
+package com.example.boot1.Explanation;
+
+/*
+Jar- Java Archive ; libraries used in dev
+Var -
+Maven:
+>basically a tool(repository) like gradle to use in dev , to add/manage external dependencies
+>
+ */
